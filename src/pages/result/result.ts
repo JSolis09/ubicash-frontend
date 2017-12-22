f5ab408a463f7f66b5220dd6794cbf126d538a0f
Jesus Solis <jsolismu@everis.com>
2017-12-22 17:19:55 -0500

    add infinite scroll

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -14,12 +14,16 @@ import { UtilProvider } from '../../providers/util/util';
 })
 export class ResultPage {
     private myLocation: Coordinates;
+    private bankList: Bank[];
     public bankId: string;
     public bank: Bank;
     public banks: Observable<Bank[]>;
     public bankDetails: BankDetail[];
+    public bankDetailList: BankDetail[];
     public bankDetail: BankDetail;
     public typeView: boolean = false;
+    private limit: number = 10;
+    private skip: number = 0;
 
     constructor(private alertCtrl: AlertController,
                 private navParams: NavParams,
@@ -27,9 +31,11 @@ export class ResultPage {
                 private utilProvider: UtilProvider,
                 private bankService: BankServiceProvider) {
         this.bankDetails = [];
+        this.bankDetailList = [];
         this.banks = this.bankService.getAll();
         this.banks
             .subscribe((banks) => {
+                this.bankList = banks;
                 this.bank = this.navParams.get('bank');
                 this.bankId = this.bank.id;
                 this.utilProvider
@@ -46,12 +52,14 @@ export class ResultPage {
             content: 'Buscando ...'
         });
         loading.present();
+        this.resetBankDetailList();
         this.bankService
             .getBankDetails(bankId)
             .subscribe((bankDetails: BankDetail[]) => {
                 const bank = Object.assign({}, this.bank);
                 this.bankDetails = bankDetails || [];
                 this.mapBankList(this.bankDetails, bank);
+                this.getBankDetailList();
                 loading.dismiss();
                 if (this.bankDetails.length === 0) {
                     const alert = this.alertCtrl.create({
@@ -86,8 +94,31 @@ export class ResultPage {
         });
     }
 
+    private resetBankDetailList(): void {
+        this.skip = 0;
+        this.bankDetailList = [];
+    }
+
+    private getBankDetailList(): void {
+        this.bankDetailList = this.bankDetails.slice(this.skip, this.limit);
+        this.skip = this.limit;
+    }
+
     public changeBank(): void {
+        this.bank = this.bankService.getBankById(this.bankList, this.bankId);
         this.getBankDetails(this.bankId);
+    }
+
+    public getMoreBankDetail(infiniteScroll: any): void {
+        if (this.skip >= this.bankDetails.length) {
+            infiniteScroll.complete();
+        }
+        setTimeout(() => {
+            const newBankDetailList = this.bankDetails.slice(this.skip, this.skip + this.limit);
+            this.bankDetailList = this.bankDetailList.concat(newBankDetailList);
+            this.skip += newBankDetailList.length;
+            infiniteScroll.complete();
+        }, 500);
     }
 
 }
